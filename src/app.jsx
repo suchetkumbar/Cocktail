a942e9b87e48e1bad933fa4babfb2832b2851ce7
BrownieByte <suchetkumbar@gmail.com>
2026-01-07 20:55:48 +0530

    Contact section added

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,7 @@ import Cocktails from './components/Cocktails';
 import About from './components/About';
 import Art from './components/Art';
 import Menu from './components/Menu';
+import Contact from './components/Contact';
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
@@ -18,7 +19,7 @@ const App = () => {
       <About />
       <Art />
       <Menu />
-      <div className='h-dvh bg-black'></div>
+      <Contact />
     </main>
   )
 }
