629c8d25b8be0960682c7989cba2462c7d2514c1
BrownieByte <suchetkumbar@gmail.com>
2026-01-07 20:26:17 +0530

    Menu portion in progress

---
+++ src/components/Menu.jsx
@@ -0,0 +1,29 @@
+import {allCocktails} from "../../constants";
+import { useState } from "react";
+
+const Menu = () => {
+  return (
+    <section id="menu" aria-labelledby="menu-heading">
+        <img src="/images/slider-left-leaf.png" alt="left-leaf" id="m-left-leaf"/>
+        <img src="/images/slider-right-leaf.png" alt="right-leaf" id="m-right-leaf"/>
+        <h2 id="menu-heading" className="sr-only">
+            Cocktail Menu
+        </h2>
+
+        <nav className="cocktail-tabs" aria-label="Cocktail Navigation">
+            {allCocktails.map((cocktail,index) => {
+                const isActive = index == currentIndex;
+
+                return(
+                    <button key={cocktail.id} className={`$isActive? 'text-white border-white':'text-white/50 border-white/50'`}>
+                        {cocktail.name}
+                    </button>
+                )
+
+            })}
+        </nav>
+    </section>
+  )
+}
+
+export default Menu
